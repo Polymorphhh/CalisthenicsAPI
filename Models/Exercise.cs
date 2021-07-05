506619585e91c8fc49e1eaf515273a12c202f226
Emine Bernardone <emine.bernardone@protonmail.com>
2021-07-05 07:01:54 +0200

    Created Exercise model

---
+++ Models/Exercise.cs
@@ -0,0 +1,10 @@
+namespace CalisthenicsAPI.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Hold { get; set; }
+    }
+}
