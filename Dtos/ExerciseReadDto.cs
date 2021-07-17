adcd5e3cc2962cdcc748e5fae9256182b657fcc5
Emine Bernardone <emine.bernardone@protonmail.com>
2021-07-17 10:12:24 +0200

    Updated Dto with Json properties

--- Dtos/ExerciseReadDto.cs
+++ Dtos/ExerciseReadDto.cs
@@ -1,13 +1,19 @@
+using System.Text.Json.Serialization;
+
 namespace CalisthenicsAPI.Dtos
 {
     public class ExerciseReadDto
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
 
+        [JsonPropertyName("name")]
         public string Name { get; set; }
 
+        [JsonPropertyName("description")]
         public string Description { get; set; }
 
+        [JsonPropertyName("hold")]
         public bool Hold { get; set; }
     }
 }
